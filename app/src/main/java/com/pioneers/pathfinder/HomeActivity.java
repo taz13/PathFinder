164c8bed2e59ea7837a90dc349f39e8845c126cf
taz13 <tahseen137@gmail.com>
2015-05-31 01:47:16 -0400

    1. Added new Activity with map. 2. Added code so that when findPath button pressed mapPathFinder activity starts

--- app/src/main/java/com/pioneers/pathfinder/HomeActivity.java
+++ app/src/main/java/com/pioneers/pathfinder/HomeActivity.java
@@ -1,14 +1,16 @@
 package com.pioneers.pathfinder;
 
-import com.pioneers.pathfinder.util.SystemUiHider;
-
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
+
+import com.pioneers.pathfinder.util.SystemUiHider;
 
 
 /**
@@ -40,11 +42,38 @@ public class HomeActivity extends Activity {
      * The flags to pass to {@link SystemUiHider#getInstance}.
      */
     private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
+    private static final String START = "startPlace";
+    private static final String DEST = "destPlace";
+    Handler mHideHandler = new Handler();
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    View.OnTouchListener findPathTouchListener;
     /**
      * The instance of the {@link SystemUiHider} for this activity.
      */
     private SystemUiHider mSystemUiHider;
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mSystemUiHider.hide();
+        }
+    };
+
+    public HomeActivity() {
+        findPathTouchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (AUTO_HIDE) {
+                    delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                }
+                return false;
+            }
+
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,29 +119,22 @@ public class HomeActivity extends Activity {
                             controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
                         }
 
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
+                        // Schedule a hide().
+                        if (visible) delayedHide(AUTO_HIDE_DELAY_MILLIS);
                     }
                 });
 
         // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
+        contentView.setOnClickListener(view -> {
+            if (TOGGLE_ON_CLICK) {
+                mSystemUiHider.toggle();
+            } else mSystemUiHider.show();
         });
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.find_button).setOnTouchListener(findPathTouchListener);
     }
 
     @Override
@@ -125,30 +147,6 @@ public class HomeActivity extends Activity {
         delayedHide(100);
     }
 
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-
     /**
      * Schedules a call to hide() in [delay] milliseconds, canceling any
      * previously scheduled calls.
@@ -157,4 +155,19 @@ public class HomeActivity extends Activity {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
+
+    //onClick function for find_path button
+    public void showPath(View view) {
+        //going to mapPathFinderActivity
+        Intent intent = new Intent(this, HomeActivity.class);
+        EditText startPlaceText = (EditText) findViewById(R.id.start_place_textbox);
+        EditText destPlaceText = (EditText) findViewById(R.id.destination_place_textBox);
+
+        String startPlace = startPlaceText.getText().toString();
+        String destPlace = destPlaceText.getText().toString();
+        intent.putExtra(START, startPlace);
+        intent.putExtra(DEST, destPlace);
+        startActivity(intent);
+    }
+
 }
