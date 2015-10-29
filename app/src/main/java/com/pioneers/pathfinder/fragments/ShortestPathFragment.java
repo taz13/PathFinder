60af0447c63cc7f062d5d78939b17dae2ab6fca1
taz13 <tahseen137@gmail.com>
2015-10-29 04:17:39 -0400

    Added onclick action to button to go to android map activity

--- app/src/main/java/com/pioneers/pathfinder/fragments/ShortestPathFragment.java
+++ app/src/main/java/com/pioneers/pathfinder/fragments/ShortestPathFragment.java
@@ -1,20 +1,16 @@
 package com.pioneers.pathfinder.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.TextView;
+import android.widget.Button;
 
+import com.pioneers.pathfinder.MapsPathFinderActivity;
 import com.pioneers.pathfinder.R;
-
-import com.pioneers.pathfinder.fragments.dummy.DummyContent;
 
 /**
  * A fragment representing a list of Items.
@@ -25,39 +21,41 @@ import com.pioneers.pathfinder.fragments.dummy.DummyContent;
  * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
  * interface.
  */
-public class ShortestPathFragment extends Fragment implements AbsListView.OnItemClickListener {
-
+//public class ShortestPathFragment extends Fragment implements AbsListView.OnItemClickListener {
+public class ShortestPathFragment extends Fragment  {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private Button findShortestPath;
 
     /**
      * The fragment's ListView/GridView.
      */
-    private AbsListView mListView;
+//    private AbsListView mListView;
 
     /**
      * The Adapter which will be used to populate the ListView/GridView with
      * Views.
      */
-    private ListAdapter mAdapter;
+//    private ListAdapter mAdapter;
 
-    // TODO: Rename and change types of parameters
-    public static ShortestPathFragment newInstance(String param1, String param2) {
-        ShortestPathFragment fragment = new ShortestPathFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    // TODO: Rename and change types of parameters
+//    public static ShortestPathFragment newInstance(String param1, String param2) {
+//        ShortestPathFragment fragment = new ShortestPathFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -70,20 +68,37 @@ public class ShortestPathFragment extends Fragment implements AbsListView.OnItem
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
 
-        // TODO: Change Adapter to display your content
-        mAdapter = new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS);
+
+//        // TODO: Change Adapter to display your content
+//        mAdapter = new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
+//                android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shortestpath, container, false);
+
+        findShortestPath= (Button) view.findViewById(R.id.btnSubmit);
+
+        //setting onclick listener for find shortest path button
+
+        findShortestPath.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                System.out.println("Shortest path found");
+
+                Intent showOnMap=new Intent(getActivity(), MapsPathFinderActivity.class);
+                startActivity(showOnMap);
+            }
+        });
 
 //        // Set the adapter
 //        mListView = (AbsListView) view.findViewById(android.R.id.list);
@@ -112,27 +127,27 @@ public class ShortestPathFragment extends Fragment implements AbsListView.OnItem
         mListener = null;
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (null != mListener) {
-            // Notify the active callbacks interface (the activity, if the
-            // fragment is attached to one) that an item has been selected.
-            mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
-        }
-    }
+//    @Override
+//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        if (null != mListener) {
+//            // Notify the active callbacks interface (the activity, if the
+//            // fragment is attached to one) that an item has been selected.
+//            mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
+//        }
+//    }
 
     /**
      * The default content for this Fragment has a TextView that is shown when
      * the list is empty. If you would like to change the text, call this method
      * to supply the text it should use.
      */
-    public void setEmptyText(CharSequence emptyText) {
-        View emptyView = mListView.getEmptyView();
-
-        if (emptyView instanceof TextView) {
-            ((TextView) emptyView).setText(emptyText);
-        }
-    }
+//    public void setEmptyText(CharSequence emptyText) {
+//        View emptyView = mListView.getEmptyView();
+//
+//        if (emptyView instanceof TextView) {
+//            ((TextView) emptyView).setText(emptyText);
+//        }
+//    }
 
     /**
      * This interface must be implemented by activities that contain this
