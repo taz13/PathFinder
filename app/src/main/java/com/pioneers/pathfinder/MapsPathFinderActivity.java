af7aee10716dc994de610f978f8c1c75818047a3
taz13 <tahseen137@gmail.com>
2015-10-29 04:16:11 -0400

    Updated Google Maps Activity

--- app/src/main/java/com/pioneers/pathfinder/MapsPathFinderActivity.java
+++ app/src/main/java/com/pioneers/pathfinder/MapsPathFinderActivity.java
@@ -1,65 +1,109 @@
 package com.pioneers.pathfinder;
 
-import android.os.Bundle;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.pioneers.pathfinder.util.GMapV2Direction;
 
-public class MapsPathFinderActivity extends FragmentActivity {
+import org.w3c.dom.Document;
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+import java.util.ArrayList;
+
+public class MapsPathFinderActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_path_finder);
-        setUpMapIfNeeded();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+//        // Enabling MyLocation Layer of Google Map
+//        googleMap.setMyLocationEnabled(true);
+//
+//        // Creating a criteria object to retrieve provider
+//        Criteria criteria = new Criteria();
+//
+//        // Getting LocationManager object from System Service LOCATION_SERVICE
+//        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//
+//        // Getting the name of the best provider
+//        String provider = locationManager.getBestProvider(criteria, true);
+//
+//        // Getting Current Location
+//        Location location = locationManager.getLastKnownLocation(provider);
+//
+//        // Getting latitude of the current location
+//        double latitude = location.getLatitude();
+//
+//        // Getting longitude of the current location
+//        double longitude = location.getLongitude();
+//
+//        // Creating a LatLng object for the current location
+//        LatLng latLng = new LatLng(latitude, longitude);
+//
+//        // Showing the current location in Google Map
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//
+//        // Zoom in the Google Map
+//        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        //For drawing direction
+
+        LatLng sourcePosition=new LatLng(23.750469, 90.382646);
+        LatLng destPosition=new LatLng(23.726729, 90.388880);
+
+        GMapV2Direction md = new GMapV2Direction();
+
+        Document doc = md.getDocument(sourcePosition, destPosition,
+                GMapV2Direction.MODE_DRIVING);
+
+        ArrayList<LatLng> directionPoint = md.getDirection(doc);
+        PolylineOptions rectLine = new PolylineOptions().width(3).color(
+                Color.RED);
+
+        for (int i = 0; i < directionPoint.size(); i++) {
+            rectLine.add(directionPoint.get(i));
         }
+        Polyline polylin = mMap.addPolyline(rectLine);
+
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }
+
 }
