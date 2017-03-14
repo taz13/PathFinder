856d778ae0e729a5e2625e705bbf2b2764e96796
taz13 <tahseen137@gmail.com>
2017-03-14 16:26:16 -0400

    Edited Route list view

--- app/src/main/java/com/pioneers/pathfinder/activity/ExpandableActivity.java
+++ app/src/main/java/com/pioneers/pathfinder/activity/ExpandableActivity.java
@@ -4,14 +4,24 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ExpandableListView;
 
+import com.google.firebase.appindexing.Action;
+import com.google.firebase.appindexing.FirebaseUserActions;
+import com.google.firebase.appindexing.builders.Actions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pioneers.pathfinder.R;
 import com.pioneers.pathfinder.adapter.ExpandableListAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Taslima on 10/17/2015.
@@ -23,8 +33,16 @@ public class ExpandableActivity extends Activity { // For Test Commit
     Context context;
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
+    List<String> listPathHeader;
+    HashMap<String, List<String>> listPathDetails;
+    String source;
+    String destination;
+    String busStops[];
+    ArrayList costList;
+    private DatabaseReference dbRef;
+    private DatabaseReference latLongDbRef;
+    private DatabaseReference shortestPathDbRef;
+    private Map<String, String> shortestPathMap;
 
     public static ExpandableActivity getInstance() {
         return instance;
@@ -39,89 +57,138 @@ public class ExpandableActivity extends Activity { // For Test Commit
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
         Intent intent = getIntent();
-        sourceLatitude = intent.getDoubleExtra("SourceLat", 0.0);
-        sourceLongitude = intent.getDoubleExtra("SourceLong", 0.0);
-        destinationLatitude = intent.getDoubleExtra("DestinationLat", 0.0);
-        destinationLongitude = intent.getDoubleExtra("DestinationLong", 0.0);
+        source = intent.getStringExtra("Source");
+        destination = intent.getStringExtra("Destination");
 
-        prepareListData();
-        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
-        expListView.setAdapter(listAdapter);
+
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        latLongDbRef = dbRef.child("LatLong");
+        shortestPathDbRef = dbRef.child("ShortestPaths").child(source + ":" + destination);
+
+        //DB Ref for shortest path
+        shortestPathDbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                shortestPathMap = (Map<String, String>) dataSnapshot.getValue();
+                prepareListData();
+                listAdapter = new ExpandableListAdapter(getBaseContext(), listPathHeader, listPathDetails);
+                expListView.setAdapter(listAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("DBREF", "Failed to read Shortest path list", databaseError.toException());
+            }
+        });
 
 
     }
 
     private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
+        listPathDetails = new HashMap<String, List<String>>();
+        listPathHeader = new ArrayList<>();
+        //Add header
+        costList = new ArrayList<>(shortestPathMap.keySet());
 
-        // Adding child data
-        listDataHeader.add("BRTC-9");
-        listDataHeader.add("BRTC-10");
-        listDataHeader.add("7/D");
-        listDataHeader.add("2");
-
-
-        // Adding child data
-        List<String> BRTC9 = new ArrayList<String>();
-        BRTC9.add("1. Mohammadpur\n" +
-                "\n" +
-                "2. Shyamoli\n" +
-                "\n" +
-                "3. Shishu Mela\n" + "\n" +
-                "4. Agargaon\n" + "\n" +
-                "5. Mohakhali\n" + "\n" +
-                "6. Gulsan 1\n" + "\n" +
-                "7. Badda Link Road\n" + "\n" +
-                "8. Badda");
-
-        List<String> BRTC10 = new ArrayList<String>();
-        BRTC10.add("1. Mohammadpur\n" +
-                "\n" +
-                "2. Shyamoli\n" +
-                "\n" +
-                "3. Shishu Mela\n" + "\n" +
-                "4. Agargaon\n" + "\n" +
-                "5. Mohakhali\n" + "\n" +
-                "6. Gulsan 1\n" + "\n" +
-                "7. Gulsan 2\n");
-
-
-        List<String> sevend = new ArrayList<String>();
-        sevend.add("1. Gabtoli\n" +
-                "\n" +
-                "2. Mirpur 1\n" +
-                "\n" +
-                "3. Mirpur 10\n" + "\n" +
-                "4. Agargaon\n" + "\n" +
-                "5. Mohakhali\n" + "\n" +
-                "6. Kakoli\n" + "\n" +
-                "7. Uttara\n" + "\n" +
-                "8. Abdullahpur");
-
-        List<String> two = new ArrayList<String>();
-        two.add("1. Kamalapur\n" +
-                "\n" +
-                "2. Razarbag Police Line\n" +
-                "\n" +
-                "3. Maghbazar\n" + "\n" +
-                "4. Bangla Motor\n" + "\n" +
-                "5. Farmgate\n" + "\n" +
-                "6. Agargaon\n" + "\n" +
-                "7. Shewrapara\n" + "\n" +
-                "8. Kazipara\n" + "\n" +
-                "9. Mirpur 10\n" + "\n" +
-                "10. Mirpur 2\n" + "\n" +
-                "11. Mirpur 1\n" + "\n" +
-                "12. Zoo");
-
-
-        listDataChild.put(listDataHeader.get(0), BRTC9);
-        listDataChild.put(listDataHeader.get(1), BRTC10);
-        listDataChild.put(listDataHeader.get(2), sevend);
-        listDataChild.put(listDataHeader.get(3), two);// Header, Child data
+        for (int i = 0; i < shortestPathMap.size(); i++) {
+            listPathHeader.add("Route " + (i + 1));
+            busStops = shortestPathMap.get(costList.get(i)).split(":");
+            StringBuilder buildRoute = new StringBuilder("");
+            buildRoute.append("Cost: " + costList.get(i) + "\n");
+            buildRoute.append("Number of stops: " + busStops.length + "\n");
+            buildRoute.append("Route:\n");
+            for (int j = 0; j < busStops.length; j++) {
+                buildRoute.append("\t" + (j + 1) + ". " + busStops[j] + "\n");
+            }
+            ArrayList temp = new ArrayList();
+            temp.add(buildRoute.toString());
+            listPathDetails.put(listPathHeader.get(i), temp);
+        }
+//        // Adding child data
+//        List<String> BRTC9 = new ArrayList<String>();
+//        BRTC9.add("1. Mohammadpur\n" +
+//                "\n" +
+//                "2. Shyamoli\n" +
+//                "\n" +
+//                "3. Shishu Mela\n" + "\n" +
+//                "4. Agargaon\n" + "\n" +
+//                "5. Mohakhali\n" + "\n" +
+//                "6. Gulsan 1\n" + "\n" +
+//                "7. Badda Link Road\n" + "\n" +
+//                "8. Badda");
+//
+//        List<String> BRTC10 = new ArrayList<String>();
+//        BRTC9.add("1. Mohammadpur\n" +
+//                "\n" +
+//                "2. Shyamoli\n" +
+//                "\n" +
+//                "3. Shishu Mela\n" + "\n" +
+//                "4. Agargaon\n" + "\n" +
+//                "5. Mohakhali\n" + "\n" +
+//                "6. Gulsan 1\n" + "\n" +
+//                "7. Gulsan 2\n");
+//
+//
+//        List<String> sevend = new ArrayList<String>();
+//        sevend.add("1. Gabtoli\n" +
+//                "\n" +
+//                "2. Mirpur 1\n" +
+//                "\n" +
+//                "3. Mirpur 10\n" + "\n" +
+//                "4. Agargaon\n" + "\n" +
+//                "5. Mohakhali\n" + "\n" +
+//                "6. Kakoli\n" + "\n" +
+//                "7. Uttara\n" + "\n" +
+//                "8. Abdullahpur");
+//
+//        List<String> two = new ArrayList<String>();
+//        two.add("1. Kamalapur\n" +
+//                "\n" +
+//                "2. Razarbag Police Line\n" +
+//                "\n" +
+//                "3. Maghbazar\n" + "\n" +
+//                "4. Bangla Motor\n" + "\n" +
+//                "5. Farmgate\n" + "\n" +
+//                "6. Agargaon\n" + "\n" +
+//                "7. Shewrapara\n" + "\n" +
+//                "8. Kazipara\n" + "\n" +
+//                "9. Mirpur 10\n" + "\n" +
+//                "10. Mirpur 2\n" + "\n" +
+//                "11. Mirpur 1\n" + "\n" +
+//                "12. Zoo");
+//
+//
+//        listPathDetails.put(listPathHeader.get(0), BRTC9);
+//        listPathDetails.put(listPathHeader.get(1), BRTC10);
+//        listPathDetails.put(listPathHeader.get(2), sevend);
+//        listPathDetails.put(listPathHeader.get(3), two);// Header, Child data
 
 
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        return Actions.newView("Expandable", "http://[ENTER-YOUR-URL-HERE]");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        FirebaseUserActions.getInstance().start(getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        FirebaseUserActions.getInstance().end(getIndexApiAction());
+        super.onStop();
+    }
 }
